6982c14159cc5069a17df53f4eeb8eb893b12826
Adriano Mendes <adriano.mendes@class-solutions.com.br>
2016-05-19 17:34:22 -0300

    .

---
+++ LAB.BLL/Clientes.cs
@@ -0,0 +1,35 @@
+﻿using LAB.BLL.Interface;
+using LAB.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB.BLL
+{
+    class Clientes : IClientes
+    {
+        List<ClientesInfo> clientes = new List<ClientesInfo>();
+        public void AddCliente()
+        {
+            ClientesInfo c1 = new ClientesInfo(1, "David");
+            ClientesInfo c2 = new ClientesInfo(2, "Adriano");
+            ClientesInfo c3 = new ClientesInfo(3, "Itamar");
+            ClientesInfo c4 = new ClientesInfo(4, "Lucas");
+            clientes.Add(c1);
+            clientes.Add(c2);
+            clientes.Add(c3);
+            clientes.Add(c4);
+        }
+        public void DeleteCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<string> IClientes.GetAllClientes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
